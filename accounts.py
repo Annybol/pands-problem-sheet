c1ed55248b2596a85f5fea6b9c468a563b1faa81
Annybol <123621445+Annybol@users.noreply.github.com>
2023-02-23 17:20:43 +0000

    Add files via upload

---
+++ accounts.py
@@ -0,0 +1,11 @@
+#accounts.py
+#Entering number and outputs  the number with just last 4 digits showing
+#Anne Boland
+
+
+x = (input("Please enter your account number: "))
+
+
+print(f"Is your account number ending in xxxxxx{x[6:10]}")
+
+
