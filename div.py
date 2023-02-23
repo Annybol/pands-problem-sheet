448753d9494ea90e98b01d1a26447c3ef60697c2
Annybol <123621445+Annybol@users.noreply.github.com>
2023-02-23 02:12:25 +0000

    Add files via upload

---
+++ div.py
@@ -0,0 +1,14 @@
+# div.py
+# input two numbers divided by eachother
+# Output the integer with remainder
+# Anne Boland
+
+x = int(input("enter the first number:"))
+y = int(input("enter second number:"))
+answer = (x//y)
+
+remainder = x%y
+
+print (f"answer is {answer} with {remainder} remaining")
+
+       
