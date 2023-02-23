651b6a418c2b08d1237862149d1c92909ba53e33
Annybol <123621445+Annybol@users.noreply.github.com>
2023-02-23 02:11:35 +0000

    Add files via upload

---
+++ sub.py
@@ -0,0 +1,9 @@
+# syb.py
+# reading two numbers and subtracting one from the other
+# Anne Boland
+
+x = int (input("enter first number:"))
+y = int (input("enter second number:"))
+answer =(x-y)
+
+print (f"answer is {answer}")
