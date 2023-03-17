72df658c2bdc36fdb0de93bad3fbcb19d83bb28d
Annybol <123621445+Annybol@users.noreply.github.com>
2023-03-17 16:56:54 +0000

    Update bank.py

--- bank.py
+++ bank.py
@@ -6,7 +6,7 @@ x = int(input("enter amount1(in cent): "))
 y = int(input("enter amount2(in cent): "))
 
 ans = (x/100) + (y/100)
-# I want to show to two decimal points
+# I want to show to two decimal points from w3schools
 txt = "the total sum is ${:.2f}"
 
 print (txt.format(ans))
