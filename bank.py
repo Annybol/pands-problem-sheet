d81a099f60d661fe28cbb428360c659a4f9bdc2a
Annybol <123621445+Annybol@users.noreply.github.com>
2023-03-17 16:55:56 +0000

    Update bank.py

--- bank.py
+++ bank.py
@@ -6,8 +6,8 @@ x = int(input("enter amount1(in cent): "))
 y = int(input("enter amount2(in cent): "))
 
 ans = (x/100) + (y/100)
-
-txt = "the total sum i ${:.2f}"
+# I want to show to two decimal points
+txt = "the total sum is ${:.2f}"
 
 print (txt.format(ans))
 
